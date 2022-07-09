0c606d193ebe5d94fd3d36914826f7f05aeeb98b
Rafael Furtado <rafs.97@hotmail.com>
2022-07-09 16:48:06 -0300

    Configuração inicial

---
+++ src/exercise5/carsCrud/src/main/java/tinnova/carsCrud/CarsCrudApplication.java
@@ -0,0 +1,13 @@
+package tinnova.carsCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsCrudApplication.class, args);
+	}
+
+}
---
+++ src/exercise5/carsCrud/src/test/java/tinnova/carsCrud/CarsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package tinnova.carsCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
